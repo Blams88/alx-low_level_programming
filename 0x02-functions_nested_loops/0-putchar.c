99a58c7a7f293d8c037f685165a6021c77a7b92f
Blams88 <blami@outlook.com>
2022-10-21 09:00:17 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,6 +3,7 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
+ *
  */	
 int main(void)
 {
@@ -16,7 +17,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
-
-return (0);
+        
+	return (0);
 }
