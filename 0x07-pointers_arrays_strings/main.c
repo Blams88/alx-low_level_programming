d2d78f2d525dee98bb06f761af43b03bc4ebfb10
Blams88 <109959215+Blams88@users.noreply.github.com>
2022-11-07 19:04:33 +0000

    Update main.c

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -10,5 +11,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
+
 #endif 
 
