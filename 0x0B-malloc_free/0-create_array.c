c51c08226881ebddffa9691666bb10d7118b43c0
Blams88 <blami@outlook.com>
2022-11-08 03:58:02 -0800

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,17 +10,16 @@ char *create_array(unsigned int size, char c)
 {
         unsigned int i;
         char *array;
-        if (size == 0)
-                return (NULL);
-        array = malloc(size * sizeof(char));
-        if (array == NULL)
-        {
-                return (NULL);
-        }
-        for (i = 0; i < size; i++)
-        {
-                array[i] = c;
-        }
-        return (array);
+	if (size == 0)
+		return (NULL);
+	array = malloc(size * sizeof(char));
+	if (array == NULL){
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
 }
 
