74f9e5b9d693d9c678f52aeac700e070dd096651
Blams88 <blami@outlook.com>
2022-11-08 08:13:05 -0800

    updatz

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main -prints the name of the program
+*@arg
+*@argv
+*/
+int main(int argc_attribute_((unused)),char *argv[])
+{ 
+printf ("%s\n",argv[0]);
+return(0);
+}
+
