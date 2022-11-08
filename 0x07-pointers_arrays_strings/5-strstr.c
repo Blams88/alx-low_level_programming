618f278b0d67d6e97b7e8cc65e9fbac2d1461134
Blams88 <109959215+Blams88@users.noreply.github.com>
2022-11-08 08:41:16 +0000

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,29 +1,31 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stddef.h>
 /**
- * _strstr - Entry point
- * @haystack: input
- * @needle: input
- * Return: Always 0 (Success)
- */
+  * _strstr - finds the first occurence of needle in haystack
+  * @haystack: where we look
+  * @needle: what we look for
+  *
+  *
+  * Return: pointer to beginning of located substring or NULL
+  *
+  */
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack != '\0'; haystack++)
+	int point;
+
+	for (; *haystack; haystack++)
 	{
-		char *one = haystack;
-		char *two = needle;
-
-		while (*one == *two && *two != '\0')
+		for (point = 0; needle[point]; point++)
 		{
-			one++;
-			two++;
+			if (!(*(haystack + point)))
+			{
+				return (NULL);
+			}
+		if (*(haystack + point) != needle[point])
+			break;
 		}
-
-		if (*two == '\0')
+		if (needle[point] == '\0')
 			return (haystack);
 	}
-
 	return (NULL);
 }
-
