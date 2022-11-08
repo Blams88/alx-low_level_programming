a81a35cacaf7cecb12030af95ab9299169fc2135
Blams88 <blami@outlook.com>
2022-11-08 00:59:20 -0800

    update code

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
   * _memset - fills memory with a constant byte
   * @s: memory area
@@ -6,10 +7,13 @@
   * @n: # of bytes to fill
   * Return: pointer to s
   */
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n}
 {
 	char *ptr = s;
 	while (n-- > 0)
 	*ptr++ = b;
 	return (s);
+
+
 }
+
