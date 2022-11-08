ddfb60ed5d74ebed1a24604096b914d79fc184fb
Blams88 <blami@outlook.com>
2022-11-08 02:21:26 -0800

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,10 +9,9 @@ void _puts_recursion(char *s)
 if (*s)
 {
 	_putchar(*s);
-	 _puts_recursion(s + 1);
+	_puts_recursion(s + 1);
 }
-	 if (!*s){
-	 	_putchar('\n');
-	 }
+	if (!*s)
+	_putchar('\n'); 
 }
 
