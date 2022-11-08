d74c6769429f9f903299c118970f5077325930af
Blams88 <blami@outlook.com>
2022-11-08 05:36:41 -0800

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,26 +7,26 @@
  */
 char *_strdup(char *str)
 {
-	unsigned int i;
-	int count = 0;
-	char *dest;
-	if (str == NULL)
-		return (NULL);
-	for (i = 0; str[i]; i++)
-	{
-		count++;
-	}
-	count += 1;
-	dest = malloc(count * sizeof(char));
-	if (dest == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		dest[i] = str[i];
-	}
+unsigned int i;
+int count = 0;
+char *dest;
+if (str == NULL)
+	return (NULL);
+for (i = 0; str[i]; i++)
+{
+	count++;
+}
+count += 1;
+dest = malloc(count * sizeof(char));
+if (dest == NULL)
+{
+	return (NULL);
+}
+for (i = 0; str[i] != '\0'; i++)
+{
 	dest[i] = str[i];
-	return (dest);
+}
+dest[i] = str[i];
+return (dest);
 }
 
