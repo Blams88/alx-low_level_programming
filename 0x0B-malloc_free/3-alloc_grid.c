71cd56a4eab5e8b82f093d6c36af2dcaaef032be
Blams88 <blami@outlook.com>
2022-11-08 07:23:30 -0800

    update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,30 +14,28 @@ int y;
 int i;
 int **arr;
 if (width < 1 || height < 1)
-return (NULL);
+    return (NULL);
 arr = malloc(sizeof(int *) * height);
 if (arr == NULL)
 	return (NULL);
 for (i = 0; i < height; i++)
 {
 	arr[i] = malloc(sizeof(int) * width);
-	if (arr[i] == NULL)
+if (arr[i] == NULL)
+{
+	for (freenum = 0; freenum < i; freenum++)
 	{
-		for (freenum = 0; freenum < i; freenum++)
-		{
-			free(arr[freenum]);
-		}
-		free(arr);
-		return (NULL);
-	}
-	for (x = 0; x < height; x++)
+		free(arr[freenum]);
+    }
+	free(arr);
+	return (NULL);
+}
+for (x = 0; x < height; x++)
+{
+	for (y = 0; y < width; y++)
 	{
-		for (y = 0; y < width; y++)
-		{
-			arr[x][y] = 0;
-		}
+		arr[x][y] = 0;
 	}
 }
 return (arr);
 }
-
