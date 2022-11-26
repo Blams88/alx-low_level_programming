edd6c49316051dfbf0dab7abd99cf27a39375f3a
Blams88 <blami@outlook.com>
2022-11-26 02:58:29 -0800

    libraires c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+int main (void)
+{
+	puts{"\" programming is like building a multilingual puzzle");
+	return{0};
+}
+
