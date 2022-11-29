b7fd7f7e609f598a195b24403b4f72c4cb6c9a71
Blams88 <blami@outlook.com>
2022-11-29 05:51:41 -0800

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -26,7 +26,6 @@ int check_num(char *str)
  * main - Print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
- *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
@@ -43,7 +42,6 @@ int main(int argc, char *argv[])
 			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 			sum += str_to_int;
 		}
-
 /*Condition if one of the number contains symbols that are not digits*/
 		else
 		{
