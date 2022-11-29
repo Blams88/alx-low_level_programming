61dfdaed198a60ace8d4d8c26b6805036ef958ef
Blams88 <blami@outlook.com>
2022-11-29 06:02:42 -0800

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 	int str_to_int;
 	int sum = 0;
 	count = 1;
-	while (count < argc) /*Goes through the whole array*/
+	while (count< argc)/*Goes through the whole array*/
 	{
 		if (check_num(argv[count]))
 		{
